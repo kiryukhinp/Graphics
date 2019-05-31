0e01f1e552bb1a81f30b04a06fc1005255843bf8
kiryukhinp <kiryukhinp@inbox.ru>
2019-05-31 20:39:11 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -12,18 +12,21 @@ void reshape(int w,int h)
 
     glLoadIdentity();
 }
-void display()
+void square()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_QUADS);
-    glColor3b(255,0,0);
-    glVertex2i(250,450);
-    glColor3b(0,0,255);
-    glVertex2i(250,150);
-    glColor3b(0,255,0);
-    glVertex2i(550,450);
+    glBegin(GL_LINE_STRIP);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(200, 200);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(200, 400);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(400, 400);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(400, 200);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(200, 200);
     glEnd();
-
     glutSwapBuffers();
 }
 int main(int argc, char* argv[])
@@ -33,7 +36,7 @@ int main(int argc, char* argv[])
     glutInitWindowSize(800,600);
     glutCreateWindow("hui");
     glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
+    glutDisplayFunc(square);
 
     glutMainLoop();
 
